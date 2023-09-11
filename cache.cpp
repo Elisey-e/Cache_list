65a1a4da32961815caa3c031553ce1da7fb5bc45
Елисей <zhdanov.ea@phystech.edu>
2023-09-11 23:47:01 +0300

    Big fix algoritmic bugs

--- cache.cpp
+++ cache.cpp
@@ -8,6 +8,8 @@
 
 int hash_f(int x);
 
+int perfect_cache(int size_of_cache, int el_count);
+
 using std::cin, std::cout, std::endl, std::unordered_map, std::vector, std::list, std::set;
 
 #define ONE_USED  1
@@ -17,7 +19,64 @@ int hash_f(int x){
     return x % 1000;
 }
 
+int perfect_cache(int size_of_cache, int el_count){
+    unordered_map<int, list<int>> elements_places = {};
+    vector <int> data = {1, 2, 5, 5, 5, 1};
+
+    for (int i = 0; i < el_count; ++i){                    
+        elements_places[data[i]].push_back(i);
+    }
+
+    vector <int> buff = {};
+    int hit = 0;
+    for (auto i : data){
+        elements_places[i].pop_front();
+        if ((int) buff.size() < size_of_cache){
+            if (std::find(buff.begin(), buff.end(), i) != buff.end()){
+                ++hit;
+            }
+            else{
+                buff.push_back(i);
+            }
+        }
+        else{
+            if (std::find(buff.begin(), buff.end(), i) != buff.end()){
+                ++hit;
+            }
+            else{
+                //int minimum_pos = (int) buff.size() + 1;           
+                int maximum_pos = 0;
+                auto el_with_max_pos = buff[0];
+                for (auto j : buff){
+                    if (elements_places[j].size() != 0){
+                        //minimum_pos = std::min(elements_places[j].front(), minimum_pos);
+                        if (elements_places[j].front() > maximum_pos){
+                            el_with_max_pos = j;
+                            maximum_pos = elements_places[j].front();
+                        }
+                    }
+                    else{
+                        maximum_pos = (int) buff.size();
+                        el_with_max_pos = j;
+                        buff.insert(std::find(buff.begin(), buff.end(), el_with_max_pos), i);
+                        break;
+                    }
+                }
+                if (elements_places[i].front() < maximum_pos){
+                    buff.insert(std::find(buff.begin(), buff.end(), el_with_max_pos), i);
+                    buff.erase(std::find(buff.begin(), buff.end(), el_with_max_pos));
+                }
+            }
+        }
+    }
+    cout << hit << endl;
+    return 0;
+    
+}
+
 int main(){
+    perfect_cache(2, 6);
+    return 0;
 
     unordered_map<int, list<int>> memc = {};
     unordered_map<int, set<int>> hash_table = {};
